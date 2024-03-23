203f4a1f4d2bbe0fc860d16a1b724afcd22b8ee6
iVladyuser <korol.v09@gmail.com>
2024-03-23 23:51:45 +0100

    add tailwind css, add background, add status label (with disabled prop) using clsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import StatusLabel, { Status } from '@/app/components/status-label';
+
 export default function Home() {
-	return (
-		<main>
-			<h1>Home page</h1>
-		</main>
-	);
+  return (
+    <main>
+      <h1 className="text-xl">Home page</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+    </main>
+  );
 }
