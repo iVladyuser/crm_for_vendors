110c3f03b7c91f58503b713149eea60d323df2bf
iVladyuser <korol.v09@gmail.com>
2024-03-26 12:18:47 +0100

    Add link and hooks for navigation

--- src/app/components/sidebar-item.tsx
+++ src/app/components/sidebar-item.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import clsx from "clsx";
 
 export interface SidebarItemProps {
-  pathname: string;
-  src: string;
-  alt: string;
-  children: React.ReactNode;
+	current?: boolean;
+	pathname: string;
+	src: string;
+	alt: string;
+	children: React.ReactNode;
 }
 
 export default function SidebarItem({
-  pathname,
-  src,
-  alt,
-  children,
+	current,
+	pathname,
+	src,
+	alt,
+	children,
 }: SidebarItemProps) {
-  return (
-    <li>
-      <a href={pathname} className="flex items-center h-9 mx-1 gap-3.5">
-        <Image className="ml-5" width={18} height={18} src={src} alt={alt} />
-        <span className="font-medium text-zinc-50">{children}</span>
-      </a>
-    </li>
-  );
+	return (
+		<li>
+			<Link
+				href={pathname}
+				className={clsx(
+					"flex items-center h-9 mx-1 gap-3.5",
+					current &&
+						"after:h-full after:ml-auto after:border-2 after:border-purple-200 after:rounded-sm"
+				)}>
+				<Image
+					className="ml-5"
+					width={18}
+					height={18}
+					src={src}
+					alt={alt}
+				/>
+				<span className="font-medium text-zinc-50">{children}</span>
+			</Link>
+		</li>
+	);
 }
