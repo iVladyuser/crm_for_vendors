0881f67f157c02cac5123e6b647de774ea636f91
iVladyuser <korol.v09@gmail.com>
2024-03-28 00:01:03 +0100

    Add new company page

--- src/app/(admin)/companies/[id]/not-found.tsx
+++ src/app/(admin)/companies/[id]/not-found.tsx
@@ -1,19 +1,17 @@
-"use client";
+'use client';
 
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
 
 export interface NotFoundProps {}
 
 export default function NotFound({}: NotFoundProps) {
-	return (
-		<div>
-			<p>Could not found company</p>
-			<Link
-				href="/companies"
-				className="text-blue-500">
-				Back to companies
-			</Link>
-		</div>
-	);
+  return (
+    <div>
+      <p>Could not found company</p>
+      <Link href="/companies" className="text-blue-500">
+        Back to companies
+      </Link>
+    </div>
+  );
 }
--- src/app/components/add-company-button.tsx
+++ src/app/components/add-company-button.tsx
@@ -1,23 +1,14 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import dynamic from 'next/dynamic';
-import Button from '@/app/components/button';
-
-const CompanyFormModal = dynamic(() => import('./company-form-modal'), {
-  ssr: false,
-});
+import React from "react";
+import { useRouter } from "next/navigation";
+import Button from "@/app/components/button";
 
 export default function AddCompanyButton() {
-  const [show, setShow] = useState(false);
-  return (
-    <>
-      <Button onClick={() => setShow(true)}>Add company</Button>
-      <CompanyFormModal
-        onSubmit={console.log}
-        show={show}
-        onClose={() => setShow(false)}
-      />
-    </>
-  );
+	const router = useRouter();
+	return (
+		<Button onClick={() => router.push("/companies/new")}>
+			Add company
+		</Button>
+	);
 }
