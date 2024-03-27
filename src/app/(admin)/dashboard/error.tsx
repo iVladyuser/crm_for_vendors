435119f56719220365f3ecdd27558a6c4edaf487
iVladyuser <korol.v09@gmail.com>
2024-03-27 09:47:34 +0100

    Add dashboard slots

--- src/app/(admin)/dashboard/error.tsx
+++ src/app/(admin)/dashboard/error.tsx
@@ -1,18 +1,18 @@
-"use client";
+'use client';
 
-import React from "react";
-import Button from "@/app/components/button";
+import React from 'react';
+import Button from '@/app/components/button';
 
 export interface ErrorComponentProps {
-	error: Error;
-	reset: () => void;
+  error: Error;
+  reset: () => void;
 }
 
 export default function ErrorComponent({ error, reset }: ErrorComponentProps) {
-	return (
-		<div>
-			<p>{`Something went wrong. ${error.message}`}</p>
-			<Button onClick={() => reset()}>Try again</Button>
-		</div>
-	);
+  return (
+    <div>
+      <p>{`Something went wrong. ${error.message}`}</p>
+      <Button onClick={() => reset()}>Try again</Button>
+    </div>
+  );
 }
