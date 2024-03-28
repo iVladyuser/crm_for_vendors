79842f177032f752810a4435209fcf13d1347492
iVladyuser <korol.v09@gmail.com>
2024-03-28 08:39:23 +0100

    Add using fetch

--- src/app/(admin)/dashboard/@countries/page.tsx
+++ src/app/(admin)/dashboard/@countries/page.tsx
@@ -1,30 +1,38 @@
-import React from 'react';
-import Image from 'next/image';
-import clsx from 'clsx';
-import { getSummaryCountries } from '@/lib/api';
-import DashboardCard from '@/app/components/dashboard-card';
+import React from "react";
+import Image from "next/image";
+import clsx from "clsx";
+import { getCompanies, getCountries } from "@/lib/api";
+import DashboardCard from "@/app/components/dashboard-card";
+import getCountById from "@/lib/utils/getCountById";
 
 export interface PageProps {}
 
 export default async function Page({}: PageProps) {
-  const data = await getSummaryCountries();
+	const countries = await getCountries();
+	const companies = await getCompanies();
 
-  return (
-    <DashboardCard label="Countries of companies">
-      <div className="flex items-end pb-5 px-5 gap-2">
-        <div>
-          {data.map(({ countryId, countryTitle, count }) => (
-            <p
-              key={countryId}
-              className={clsx(
-                'text-sm text-gray-900 font-medium',
-                'before:inline-block before:w-2 before:h-2 before:rounded-full before:align-middle before:mr-2 before:bg-purple-200',
-              )}
-            >{`${countryTitle} - ${count}`}</p>
-          ))}
-        </div>
-        <Image width={395} height={262} src="/images/world.svg" alt="world" />
-      </div>
-    </DashboardCard>
-  );
+	const counts = getCountById(companies, "countryId");
+
+	return (
+		<DashboardCard label="Countries of companies">
+			<div className="flex items-end pb-5 px-5 gap-2">
+				<div>
+					{countries.map(({ id, title }) => (
+						<p
+							key={id}
+							className={clsx(
+								"text-sm text-gray-900 font-medium",
+								"before:inline-block before:w-2 before:h-2 before:rounded-full before:align-middle before:mr-2 before:bg-purple-200"
+							)}>{`${title} - ${counts[id] || 0}`}</p>
+					))}
+				</div>
+				<Image
+					width={395}
+					height={262}
+					src="/images/world.svg"
+					alt="world"
+				/>
+			</div>
+		</DashboardCard>
+	);
 }
--- src/app/(admin)/dashboard/@promotions/page.tsx
+++ src/app/(admin)/dashboard/@promotions/page.tsx
@@ -1,34 +1,35 @@
-import React from 'react';
-import { getSummaryPromotions } from '@/lib/api';
-import SummaryTable from '@/app/components/summary-table';
-import SummaryTableHeader from '@/app/components/summary-table-header';
-import SummaryTableCell from '@/app/components/summary-table-cell';
-import DashboardCard from '@/app/components/dashboard-card';
+import React from "react";
+import { getPromotions } from "@/lib/api";
+import SummaryTable from "@/app/components/summary-table";
+import SummaryTableHeader from "@/app/components/summary-table-header";
+import SummaryTableCell from "@/app/components/summary-table-cell";
+import DashboardCard from "@/app/components/dashboard-card";
 
 export interface PageProps {}
 
 export default async function Page({}: PageProps) {
-  const data = await getSummaryPromotions();
+	const data = await getPromotions();
 
-  return (
-    <DashboardCard label="Promotions">
-      <SummaryTable
-        headers={
-          <>
-            <SummaryTableHeader>Company</SummaryTableHeader>
-            <SummaryTableHeader>Name</SummaryTableHeader>
-            <SummaryTableHeader align="center">%</SummaryTableHeader>
-          </>
-        }
-      >
-        {data.map(({ promotionId, promotionName, companyTitle, discount }) => (
-          <tr key={promotionId}>
-            <SummaryTableCell>{companyTitle}</SummaryTableCell>
-            <SummaryTableCell>{promotionName}</SummaryTableCell>
-            <SummaryTableCell align="center">{`-${discount}%`}</SummaryTableCell>
-          </tr>
-        ))}
-      </SummaryTable>
-    </DashboardCard>
-  );
+	return (
+		<DashboardCard label="Promotions">
+			<SummaryTable
+				headers={
+					<>
+						<SummaryTableHeader>Company</SummaryTableHeader>
+						<SummaryTableHeader>Name</SummaryTableHeader>
+						<SummaryTableHeader align="center">
+							%
+						</SummaryTableHeader>
+					</>
+				}>
+				{data.map(({ id, title, companyTitle, discount }) => (
+					<tr key={id}>
+						<SummaryTableCell>{companyTitle}</SummaryTableCell>
+						<SummaryTableCell>{title}</SummaryTableCell>
+						<SummaryTableCell align="center">{`-${discount}%`}</SummaryTableCell>
+					</tr>
+				))}
+			</SummaryTable>
+		</DashboardCard>
+	);
 }
